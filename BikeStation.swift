dc866e18f486cb12a260783b41541202854e38f4
Paul <paul.lintilhac@gmail.com>
2016-07-15 19:04:28 -0400

    initial commit for bikestation class

---
+++ BikeStation.swift
@@ -0,0 +1,23 @@
+class BikeStation {
+ 
+  // 2
+  let latitutde: Double
+  let longitude: Double
+  let id: int
+  let stationName: String
+  let availableDocks: int
+  let totalDocks: int
+  let statusValue: String
+  let statusKey: int
+  let availableBikes: int
+  let stAddress1: String
+  let stAddress2: String
+  let city: String
+  let testStation: bool
+
+ init(){
+//initialize with a JSON object
+}
+ 
+}
+ 
